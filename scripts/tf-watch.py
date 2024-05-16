75927dbbcad4b5af797430a39ba5d9690b68b089
Lukáš Zachar <lzachar@redhat.com>
2024-05-16 11:36:50 +0200

    more states..

--- scripts/tf-watch.py
+++ scripts/tf-watch.py
@@ -7,18 +7,21 @@ while True:
     ret = requests.get(sys.argv[1]).json()
     state = ret['state']
     print(f'{state=}', file=sys.stderr)
-    if state not in ['running']:
+    if state not in ['new', 'queued', 'running']:
+        print(f'Stop waiting, becasue {state=}', file=sys.stderr)
         break
     print(time.gmtime(), file=sys.stderr)
     time.sleep(60) 
 
 # not running...
 if state != 'complete':
+    print('Not completed job!', file=sys.stderr)
     raise SystemExit(2)
     
 overal_result = ret['result']['overall']
 print(f'{overal_result=}', file=sys.stderr)
 
 if overal_result != 'passed':
+    print('Not passed', file=sys.stderr)
     raise SystemExit(1)
 
