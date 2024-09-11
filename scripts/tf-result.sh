d6981365d20ef924d07b80626afed86f01538a71
Lukáš Zachar <lzachar@redhat.com>
2024-09-11 16:13:38 +0200

    EMOVE

--- scripts/tf-result.sh
+++ scripts/tf-result.sh
@@ -2,4 +2,4 @@
 
 # Until Konflux shows pass/fail/error directly in webui lets exit nonzero if testing has failed.
 # Easier to spot not-passing ITS this way.
-grep SUCCESS  "$1" < /dev/null
+echo $PRODUCED_TEST_OUTPUT | grep SUCCESS
